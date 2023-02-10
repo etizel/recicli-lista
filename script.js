aaa9735bd3bc523c7372af3c9226d8920c80d70b
etiiizel <franciscoetizel@gmail.com>
2023-02-10 18:37:58 -0300

    recicli v.1.3

--- script.js
+++ script.js
@@ -1,40 +1,4 @@
-// fetch('./arquivo.json')
-//   .then((response) => response.json())
-//   .then((data) => {
-//     // Criar a lista HTML
-//     const list = document.createElement('ul');
-//     const stringify = JSON.stringify(data);
-//     // Adicionar cada item do JSON à lista
-//     stringify.forEach((item) => {
-//       const li = document.createElement('li');
-//       li.innerText = item;
-//       list.appendChild(li);
-//     });
-
-//     // Adicionar a lista à página
-//     document.querySelector('#list-push').appendChild(list);
-//   });
-// async function pushData() {
-//   const dadosResponse = await fetch('./arquivo.json');
-//   const dadosJSON = await dadosResponse.json();
-//   const list = document.querySelector('#list-push');
-//   list.innerText = JSON.stringify(dadosJSON);
-//   const style = document.createElement('style');
-//   style.innerText = dadosJSON;
-// }
-
-// function regexItem() {
-//   let cleanData = this.dadosJSON.map((item) => {
-//     return {
-//       nome: item.nome,
-//       endereco: item.endereco.replace(/[^a-zA-Z0-9,. ]/g, ''),
-//     };
-//   });
-
-//   let cleanJsonString = JSON.stringify(cleanData);
-// }
-
-// pushData();
+var indicesToChangeColor = [1, 2, 3, 6, 7, 8, 21, 36];
 
 fetch('arquivo.json')
   .then((response) => response.json())
@@ -43,12 +7,20 @@ fetch('arquivo.json')
     const list = document.createElement('ul');
 
     // Adicionar cada item da lista ao HTML
-    data.forEach((item) => {
+    data.forEach((item, index) => {
       const li = document.createElement('li');
-      li.textContent = JSON.stringify(item);
       list.appendChild(li);
       li.style.listStyle = 'none';
       li.style.padding = '20px 20px';
+
+      if (indicesToChangeColor.includes(index + 1)) {
+        li.style.color = 'yellow';
+        li.style.borderColor = 'black';
+      }
+
+      li.textContent = item;
+      li.textContent = JSON.stringify(item);
+      list.appendChild(li);
     });
 
     // Adicionar a lista ao documento
