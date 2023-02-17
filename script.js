a3667cbc8fdfb053e5c4750275c60fca5681aeb8
etiiizel <franciscoetizel@gmail.com>
2023-02-17 11:11:43 -0300

    recicli-v2.9.0

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var indicesToChangeColor = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 20, 23, 26, 32, 36, 37, 38, 39,
-  40, 41,
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 20, 23, 26, 29, 32, 33, 34, 36,
+  37, 38, 39, 40, 41, 42,
 ];
-var indicesNotDefine = [14, 15, 16, 17, 24, 28];
+var indicesNotDefine = [14, 15, 16, 17, 24, 28, 31];
 
 fetch('arquivo.json')
   .then((response) => response.json())
